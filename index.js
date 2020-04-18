683458436a4f0a2a1c0fe6434ac1e8b3a0d5e6c3
Buddy Agyin <agyin3@gmail.com>
2020-04-18 16:32:04 -0600

    move array to the index.js file, add 7th task

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { players } from './data.js'
+
 
 /* OBJECTS PRACTICE */
 
@@ -29,7 +29,7 @@ function getName(/* code here */){
 }
 
 /* Uncomment the console.log() to check your work */
-// console.log(getName(resturant))
+// console.log(getName(restaurant))
 
 // Task 3 
 
@@ -155,6 +155,29 @@ LOOK THROUGH THE DATA PROVIDED IN ./data.JS. YOU'LL SEE THAT IT IS AN ARRAY OF P
 BELOW ARE SOME PRACTICE PROBLEMS TO HELP YOU REVIEW YOUR JAVASCRIPT SKILLS
 */
 
+const players = [
+    {id: 1, name: "Kobe Bryant", team: "Lakers", position: "SG", status: "retired", years_active: "1996 - 2016"},
+    {id: 2, name: "Damian Lillard", team: "Blazers", position: "PG", status: "active", years_active: "2012 - 2020"},
+    {id: 3, name: "Isiah Thomas", team: "Pistons", position: "PG", status: "retired", years_active: "1981 - 1994"},
+    {id: 4, name: "Michael Jordan", team: "Bulls", position: "SG", status: "retired", years_active: "1984 - 2003"},
+    {id: 5, name: "Joel Embiid", team: "Sixers", position: "C", status: "active", years_active: "2014 - 2020"},
+    {id: 6, name: "Lebron James", team: "Lakers", position: "PF", status: "active", years_active: "2003 - 2020"},
+    {id: 7, name: "Kawhi Leonard", team: "Clippers", position: "SF", status: "active", years_active: "2011 - 2020"},
+    {id: 8, name: "Giannis Antetokounmpo", team: "Bucks", position: "PF", status: "active", years_active: "2012 - 2020"},
+    {id: 9, name: "Patrick Ewing", team: "Knicks", position: "C", status: "retired", years_active: "1985 - 2002"},
+    {id: 10, name: "Larry Bird", team: "Celtics", position: "SF", status: "retired", years_active: "1979 - 1992"},
+    {id: 11, name: "Shaq Oneal", team: "Lakers", position: "C", status: "retired", years_active: "1992 - 2011"},
+    {id: 12, name: "Bill Russell", team: "Celtics", position: "C", status: "retired", years_active: "1956 - 1969"},
+    {id: 13, name: "Steph Curry", team: "Warriors", position: "PG", status: "active", years_active: "2009 - 2020"},
+    {id: 14, name: "James Harden", team: "Rockets", position: "PG", status: "active", years_active: "2009 - 2020"},
+    {id: 15, name: "John Wall", team: "Wizards", position: "PG", status: "active", years_active: "2010 - 2020"},
+    {id: 16, name: "Jimmy Butler", team: "Heat", position: "SF", status: "active", years_active: "2011 - 2020"},
+    {id: 17, name: "Karl Malone", team: "Jazz", position: "PF", status: "retired", years_active: "1985 - 2004"},
+    {id: 18, name: "Devin Booker", team: "Suns", position: "SG", status: "active", years_active: "2015 - 2020"},
+    {id: 19, name: "Jayson Tatum", team: "Celtics", position: "SF", status: "active", years_active: "2017 - 2020"},
+    {id: 20, name: "Magic Johnson", team: "Lakers", position: "PG", status: "retired", years_active: "1979 - 1991"},
+]
+
 // Task 1
 
 /*
@@ -240,3 +263,22 @@ let anthony = {
     years_active: '2012 - 2020'
 }
 
+// Task 7 
+
+/* 
+create a function called `removePlayer` that takes in two arguments
+1. an array
+2. the index of a player
+
+This function should remove the player at the given index from the array
+*/
+
+/* 
+STRETCH GOAL 
+
+create a function called `removePlayerById` that takes in two arguments
+1. an array
+2. the id of a player
+
+This function should remove the player with the given id from the array
+*/ 
